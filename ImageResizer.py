e0ebaafaa45819cc3114824c517a40128366189d
ChrAdrian <chr.adrian@yahoo.com>
2021-10-30 21:22:42 +0300

    Final

--- ImageResizer.py
+++ ImageResizer.py
@@ -13,15 +13,23 @@ rotation_angle = input("Please insert the rotation angle: ")
 width = input("Please insert desired width: ")
 height = input("Please insert desired height: ")
 os.chdir(rf"{folder}")
-if os.path.exists("Resized JPG") is False:
-    os.mkdir("Resized JPG")
-save_path = rf"{folder}\Resized JPG"
+# Create folder to stock processed photos
+if os.path.exists("Processed JPG") is False:
+    os.mkdir("Processed JPG")
+save_path = rf"{folder}\Processed JPG"
+# Iterate over all images in folder
 for image_name in os.listdir(folder):
-    image = Image.open(image_name)
-    processed_image = image.rotate(int(rotation_angle)).resize((int(width), int(height)))
-    processed_image.format = image.format
-    os.chdir(rf"{folder}\Resized JPG")
-    processed_image.save(image_name + ".jpg")
-    count += 1
-    print(f"Resizing {image_name} in image format: {processed_image.format}, image size: {processed_image.size}")
-    print(f"Total number of resized images: {count}.")
+    if os.path.isfile(image_name):
+        # Open image
+        image = Image.open(image_name)
+        # Process image
+        processed_image = image.rotate(int(rotation_angle)).resize((int(width), int(height)))
+        processed_image.format = image.format
+        # Select save photo directory
+        os.chdir(save_path)
+        processed_image.save(image_name + ".jpg")
+        # revert to the initial directory
+        os.chdir(rf"{folder}")
+        count += 1
+        print(f"Resizing {image_name} in image format: {processed_image.format}, image size: {processed_image.size}")
+print(f"Total number of resized images: {count}.")
