2b332f767ceb20212b3681d758a98b62dae89039
ChrAdrian <chr.adrian@yahoo.com>
2021-10-30 19:01:17 +0300

    Image Resize function + save

--- ImageResizer.py
+++ ImageResizer.py
@@ -1,14 +1,27 @@
 # Script created by Chiriac Adrian Stefan (chr.adrian@yahoo.com)
-# Brief : script to resize bulk images and save them as .JPG
+# Brief : script to resize, rotate bulk images and save them as .JPG
 
 
 from PIL import Image
 import os
+import cv2
 
+
+count = 0
 folder = input("Please provide folder path: ")
+rotation_angle = input("Please insert the rotation angle: ")
+width = input("Please insert desired width: ")
+height = input("Please insert desired height: ")
 os.chdir(rf"{folder}")
+if os.path.exists("Resized JPG") is False:
+    os.mkdir("Resized JPG")
+save_path = rf"{folder}\Resized JPG"
 for image_name in os.listdir(folder):
     image = Image.open(image_name)
-    image.thumbnail((3000, 2000))
-    image.save(f"{image_name}_resized.jpg")
-    print(f"Image format: {image.format}, image size: {image.size}")
+    processed_image = image.rotate(int(rotation_angle)).resize((int(width), int(height)))
+    processed_image.format = image.format
+    os.chdir(rf"{folder}\Resized JPG")
+    processed_image.save(image_name + ".jpg")
+    count += 1
+    print(f"Resizing {image_name} in image format: {processed_image.format}, image size: {processed_image.size}")
+    print(f"Total number of resized images: {count}.")
