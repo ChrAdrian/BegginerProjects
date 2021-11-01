c4b9ae620e8872e9fe5466b5d0292f9423245ff1
ChrAdrian <chr.adrian@yahoo.com>
2021-11-01 13:16:02 +0200

    Refactoring

--- GUI.py
+++ GUI.py
@@ -7,11 +7,11 @@ import PySimpleGUI as sg
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
 layout = [  [sg.Text('Password Encoder')],
-            [sg.Text('Please enter the initial password'), sg.InputText()],
+            [sg.Text('Please enter the initial password: '), sg.InputText()],
             [sg.Button('Ok'), sg.Button('Cancel')] ]
 
 # Create the Window
-window = sg.Window('Window Title', layout, margins=(300,150))
+window = sg.Window('Window Title', layout, margins=(120,60))
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
--- ImageResizer.py
+++ ImageResizer.py
@@ -7,29 +7,39 @@ import os
 import cv2
 
 
-count = 0
-folder = input("Please provide folder path: ")
-rotation_angle = input("Please insert the rotation angle: ")
-width = input("Please insert desired width: ")
-height = input("Please insert desired height: ")
-os.chdir(rf"{folder}")
-# Create folder to stock processed photos
-if os.path.exists("Processed JPG") is False:
-    os.mkdir("Processed JPG")
-save_path = rf"{folder}\Processed JPG"
-# Iterate over all images in folder
-for image_name in os.listdir(folder):
-    if os.path.isfile(image_name):
-        # Open image
-        image = Image.open(image_name)
-        # Process image
-        processed_image = image.rotate(int(rotation_angle)).resize((int(width), int(height)))
-        processed_image.format = image.format
-        # Select save photo directory
-        os.chdir(save_path)
-        processed_image.save(image_name + ".jpg")
-        # revert to the initial directory
-        os.chdir(rf"{folder}")
-        count += 1
-        print(f"Resizing {image_name} in image format: {processed_image.format}, image size: {processed_image.size}")
-print(f"Total number of resized images: {count}.")
+global save_path, rotation_angle, count, width, height
+
+
+def define_inputs():
+    global save_path, rotation_angle, count, width, height
+    count = 0
+    folder = input("Please provide folder path: ")
+    rotation_angle = input("Please insert the rotation angle: ")
+    width = input("Please insert desired width: ")
+    height = input("Please insert desired height: ")
+    os.chdir(rf"{folder}")
+    # Create folder to stock processed photos
+    if os.path.exists("Processed JPG") is False:
+        os.mkdir("Processed JPG")
+    save_path = rf"{folder}\Processed JPG"
+    return folder
+
+def resize_image(folder):
+    global save_path, rotation_angle, count, width, height
+    # Iterate over all images in folder
+    for image_name in os.listdir(folder):
+        if os.path.isfile(image_name):
+            # Open image
+            image = Image.open(image_name)
+            # Process image
+            processed_image = image.rotate(int(rotation_angle)).resize((int(width), int(height)))
+            processed_image.format = image.format
+            # Select save photo directory
+            os.chdir(save_path)
+            processed_image.save(image_name + ".jpg")
+            # revert to the initial directory
+            os.chdir(rf"{folder}")
+            count += 1
+            print(f"Resizing {image_name} in image format: {processed_image.format}, "
+                  f"image size: {processed_image.size}")
+    print(f"Total number of resized images: {count}.")
