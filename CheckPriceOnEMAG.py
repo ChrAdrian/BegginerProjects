38f954843b395fb5f61baa3bbc09548f05ae42de
ChrAdrian <chr.adrian@yahoo.com>
2021-11-02 22:09:08 +0200

    Check Price Final

--- CheckPriceOnEMAG.py
+++ CheckPriceOnEMAG.py
@@ -3,23 +3,44 @@
 
 
 import requests
-from glob import glob
 from bs4 import BeautifulSoup
-import pandas as pd
-from datetime import datetime
-from time import sleep
+import re
 
 
 HEADERS = {"User Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
             '(KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
 
-prod_tracker_URL = r'https://www.emag.ro/espressor-automat-miele-cm-6160-milkperfection-white-15-bar-1-8-l-' \
-                   'wificonnatct-onetouch-for-two-aromaticsystem-alb-11580780/pd/D84HT2MBM/?X-' \
-                   'Search-Id=0357319c0c5d3a33c2dc&X-Product-Id=7108689&X-Search-Page=1&X-Search-Position=2&X-' \
-                   'Section=search&X-MB=0&X-Search-Action=view'
+product_URL = r'https://www.emag.ro/consola-playstation-5-so-9396406/pd/DNKW72MBM/'
 
-# fetch the url
-page = requests.get(prod_tracker_URL, headers=HEADERS)
+# Fetch the url
+HTML_page = requests.get(product_URL, headers=HEADERS)
 
-# create the object that will contain all the info in the url
-soup = BeautifulSoup(page.content, "html.parser")
+# Create the object that will contain all the info in the url
+soup = BeautifulSoup(HTML_page.content, "html.parser")
+
+# Get product title
+product_title = str(soup.product_title).strip('<title>')
+product_title = re.sub(" - eMAG.ro</", "", product_title)
+
+# Determine deal status
+deal_status = soup.find("p", class_="product-new-price has-deal")
+if deal_status is None:
+    deal_status = "No deal"
+else:
+    deal_status = "Has deal"
+
+try:
+    if("Has deal" in deal_status):
+    # to prevent script from crashing when there isn't a price for the product
+        product_price = soup.find("p", class_="product-new-price has-deal").text
+        product_price = product_price[::-1]
+        product_price = (product_price[:6] + ',' + product_price[6:])[::-1]
+    else:
+        product_price = soup.find("p", class_="product-new-price").text
+        product_price = product_price[::-1]
+        product_price = (product_price[:6] + ',' + product_price[6:])[::-1]
+except:
+    product_price = 'No price is assigned to this product'
+
+
+print(product_price)
