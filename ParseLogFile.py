ae2813844fe660dcd6fea5ee1702a821509adc49
ChrAdrian <chr.adrian@yahoo.com>
2021-11-26 13:04:10 +0200

    + modifications

--- ParseLogFile.py
+++ ParseLogFile.py
@@ -36,17 +36,28 @@ for file_name in sorted(os.listdir(folder_path)):
     if len(timestamp_list) == 0:
         print("\n\"" + "Timestamp" + "\" is not found in \"" + file_name + "\"!")
     else:
-        timestamp_list = [s.strip() for s in timestamp_list]
+        timestamp_list = str([s.strip() for s in timestamp_list]).replace(",", "")
+        timestamp_list = timestamp_list[2:-3].replace("'", "")
+        timestamp_list = re.split(r"(\d+)", timestamp_list)
 
     if len(package_index_list) == 0:
         print("\n\"" + "Package index" + "\" is not found in \"" + file_name + "\"!")
     else:
-        package_index_list = [s.strip() for s in package_index_list]
+        package_index_list = str([s.strip() for s in package_index_list]).replace(",", "")
+        package_index_list = package_index_list[2:-3].replace("'", "")
+        package_index_list = re.split(r"(\d+)", package_index_list)
 
     data = [j for i in zip(package_index_list, timestamp_list) for j in i]
-    data = '\n'.join(str(line) for line in data)
+    data = data[:len(data) - 2]
+    # data = '\n'.join(str(line) for line in data).splitlines()
     print(data)
-
+    # count = 0
+    # for line in data:
+    #     if line.startswith('"package_index": 1,'):
+    #         print(line)
+        # count += 1
+        # if not count % 2 == 0:
+        #     print(line)
 
     # except:
     #     print("\nThe file doesn't exist!")
