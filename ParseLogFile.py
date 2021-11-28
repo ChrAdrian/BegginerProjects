097cfa072297c89e9a83af5f98d203d6a839d230
ChrAdrian <chr.adrian@yahoo.com>
2021-11-28 16:20:53 +0200

    Complete

--- ParseLogFile.py
+++ ParseLogFile.py
@@ -1,5 +1,5 @@
 # Script created by Chiriac Adrian Stefan (adrian.chiriac@tremend.com)
-# Brief : script to transform .txt file into JSON file
+# Brief : script to parse log file and determine if timestamps are correlated with the sample rates
 
 
 import os
@@ -36,63 +36,57 @@ print(f"Accepted frequency deviation range is between {min_frequency} and {max_f
 for file_name in sorted(os.listdir(folder_path)):
     print(f"Log file in process: {file_name}")
     os.chdir(folder_path)
-    try:
 
-        file_read = open(file_name, "r")
+    file_read = open(file_name, "r")
 
-        lines = file_read.readlines()
+    lines = file_read.readlines()
 
-        timestamp_list = []
-        package_type_ID_list = []
-        idx = 0
+    timestamp_list = []
+    package_type_ID_list = []
+    idx = 0
 
-        for line in lines:
+    for line in lines:
 
-            if 'timestamp' in line:
-                timestamp_list.insert(idx, line)
-                idx += 1
+        if 'timestamp' in line:
+            timestamp_list.insert(idx, line)
+            idx += 1
 
-            if 'index' in line:
-                package_type_ID_list.insert(idx, line)
+        if 'index' in line:
+            package_type_ID_list.insert(idx, line)
 
-        file_read.close()
+    file_read.close()
 
-        if len(timestamp_list) == 0:
-            print("\n\"" + "Timestamp" + "\" is not found in \"" + file_name + "\"!")
+    if len(timestamp_list) == 0:
+        print("\n\"" + "Timestamp" + "\" is not found in \"" + file_name + "\"!")
+    else:
+        timestamp_list = str([s.strip() for s in timestamp_list]).replace(",", "")
+        timestamp_list = timestamp_list[2:-2].replace("'", "")
+        timestamp_list = re.split(r"(\d+)", timestamp_list)
+
+    if len(package_type_ID_list) == 0:
+        print("\n\"" + "Signal index" + "\" is not found in \"" + file_name + "\"!")
+    else:
+        package_type_ID_list = str([s.strip() for s in package_type_ID_list]).replace(",", "")
+        package_type_ID_list = package_type_ID_list[2:-2].replace("'", "")
+        package_type_ID_list = re.split(r"(\d+)", package_type_ID_list)
+
+    data = [j for i in zip(package_type_ID_list, timestamp_list) for j in i]
+    data = data[:len(data) - 2]
+    data = [data[n + 2:n + 4] for n in range(0, len(data), 4)]
+
+    for sublist in data:
+        if package_type_ID in sublist:
+            sublist = [sublist[n + 1:n + 2] for n in range(0, len(sublist), 2)]
+            signal_data_set = set([x for y in sublist for x in y])
+            timestamp_values = int(''.join(signal_data_set))
+            timestamp_values_filtered_list.append(timestamp_values)
+            timestamp_values_filtered_list.sort(reverse=True)
+
+    timestamp_difference = [x - y for x, y in zip(timestamp_values_filtered_list, timestamp_values_filtered_list[1:])]
+    print(timestamp_difference)
+
+    for value in timestamp_difference:
+        if value not in range(min_frequency, max_frequency):
+            raise AssertionError(f"Timestamps not correlated with the sample rate! Value {value} is out of range!")
         else:
-            timestamp_list = str([s.strip() for s in timestamp_list]).replace(",", "")
-            timestamp_list = timestamp_list[2:-2].replace("'", "")
-            timestamp_list = re.split(r"(\d+)", timestamp_list)
-
-        if len(package_type_ID_list) == 0:
-            print("\n\"" + "Signal index" + "\" is not found in \"" + file_name + "\"!")
-        else:
-            package_type_ID_list = str([s.strip() for s in package_type_ID_list]).replace(",", "")
-            package_type_ID_list = package_type_ID_list[2:-2].replace("'", "")
-            package_type_ID_list = re.split(r"(\d+)", package_type_ID_list)
-
-        data = [j for i in zip(package_type_ID_list, timestamp_list) for j in i]
-        data = data[:len(data) - 2]
-        data = [data[n + 2:n + 4] for n in range(0, len(data), 4)]
-
-        for sublist in data:
-            if package_type_ID in sublist:
-                sublist = [sublist[n + 1:n + 2] for n in range(0, len(sublist), 2)]
-                signal_data_set = set([x for y in sublist for x in y])
-                timestamp_values = int(''.join(signal_data_set))
-                timestamp_values_filtered_list.append(timestamp_values)
-                timestamp_values_filtered_list.sort(reverse=True)
-
-        timestamp_difference = [x - y for x, y in zip(timestamp_values_filtered_list, timestamp_values_filtered_list[1:])]
-        print(timestamp_difference)
-
-        for value in timestamp_difference:
-            if value not in range(min_frequency, max_frequency):
-                print(f"Value out of maximum acceptable deviation range: {value}")
-                raise AssertionError(f"Timestamps not correlated with the sample rate! Value {value} is out of range!")
-            else:
-                continue
-
-
-    except:
-        print("\nThe file doesn't exist!")
+            continue
