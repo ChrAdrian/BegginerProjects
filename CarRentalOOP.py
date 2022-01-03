157e1a23c971b50acdb9dedf0662dda43c0bd399
ChrAdrian <chr.adrian@yahoo.com>
2022-01-03 21:56:53 +0200

    OOP

--- CarRentalOOP.py
+++ CarRentalOOP.py
@@ -23,8 +23,8 @@ class CarRental:
         else:
             time = datetime.datetime.now()
             print("You have rented a number of {} cars starting at {} hours".format(nr_of_cars, time.hour))
-        self.stock -= nr_of_cars
-        return now
+            self.stock -= nr_of_cars
+        return time
 
     def rent_car_by_day(self, nr_of_cars):
         if nr_of_cars < 0:
@@ -36,6 +36,50 @@ class CarRental:
         else:
             time = datetime.datetime.now()
             print("You have rented a number of {} cars on daily basis today starting at {} hours".format(nr_of_cars, time))
-        self.stock -= nr_of_cars
-        return now
+            self.stock -= nr_of_cars
+        return time
 
+    def rent_car_by_week(self, nr_of_cars):
+        if nr_of_cars < 0:
+            print("The number of cars should be positive!")
+            return None
+        elif n > self.stock:
+            print("Sorry, we currently have {} cars available!".format(self.stock))
+            return None
+        else:
+            time = datetime.datetime.now()
+            print("You have rented a number of {} cars on weekly basis today starting at {} hours".format(nr_of_cars, time))
+            self.stock -= nr_of_cars
+        return time
+
+    def return_car(self, request):
+        rental_time, rental_basis, number_of_cars = request
+        bill = 0
+
+        if rental_time != 0 and rental_basis != 0 and number_of_cars != 0:
+            self.stock += number_of_cars
+            time = datetime.datetime.now()
+            rental_period = time - rental_time
+
+            # hourly bill calculation
+            if rentalBasis == 1:
+                bill = round(rental_period.seconds / 3600) * 5 * number_of_cars
+
+            # daily bill calculation
+            elif rentalBasis == 2:
+                bill = round(rental_period.days) * 20 * number_of_cars
+
+            # weekly bill calculation
+            elif rentalBasis == 3:
+                bill = round(rental_period.days / 7) * 60 * number_of_cars
+
+            # family discount calculation
+            if (3 <= number_of_cars <= 5):
+                print("You are eligible for Family rental promotion of 30% discount")
+                bill = bill * 0.7
+                print("The total cost would be ${}".format(bill))
+                return bill
+
+            else:
+                print("You are not found in our registry.")
+                return None
